cb4d11064567455d22b0406006daa9c8f9e386df
Freja Louise Nielsen <frejaln@bmb.sdu.dk>
2024-12-11 11:19:12 +0100

    updates table

---
+++ doc/notes.R
@@ -0,0 +1,13 @@
+# usethis as a workflow dependency when you work in the console
+# workflow dependency: used in the console or in the palette and not in the script
+
+#Style all files in a project
+styler::style_dir()
+
+#Targets is a build dependency as we will have a target file both will be used a lot in the console
+
+#Tidyverse is a set of packages, and can therefore not be stored as a dependency, but we can load it specific
+
+#It runs the setup code chunk each time you restart
+
+#to add a package to the description file: use_package("")
