536e64e237c41135738162f7b507552b06cf23d1
Freja Louise Nielsen <frejaln@bmb.sdu.dk>
2024-12-11 10:05:26 +0100

    Making function for plotting of histogram

--- R/functions.R
+++ R/functions.R
@@ -18,3 +18,17 @@ descriptive_stats <- function(data) {
             ~base::round(.x, digits=1)
         ))
 }
+
+
+#' Plot for basic distribution of metabolite data.
+#'
+#' @param data The lipidomics dataset.
+#'
+#' @return A ggplot2 graph.
+#'
+plot_distributions <- function(data) {
+    data |>
+        ggplot2::ggplot(ggplot2::aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
