09994bc4750a572345be429aaf60efea065ca104
Freja Louise Nielsen <frejaln@bmb.sdu.dk>
2024-12-11 16:08:42 +0100

    making a fitted model for statictics analyses

--- R/functions.R
+++ R/functions.R
@@ -28,8 +28,85 @@ descriptive_stats <- function(data) {
 #' @return A ggplot2 graph.
 #'
 plot_distributions <- function(data) {
-  data |>
+  data %>%
     ggplot2::ggplot(ggplot2::aes(x = value)) +
     ggplot2::geom_histogram() +
     ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
+
+
+#' Convert a column's character values to snakecase
+#'
+#' @param data The lipdomics dataset
+#' @param columns The column you want to use snakecase for
+#'
+#' @return Dataframe
+#'
+column_values_to_snake_case <-
+  function(data, columns) {
+    data %>% dplyr::mutate(dplyr::across({{ columns }}, snakecase::to_snake_case))
+  }
+
+
+
+
+#' Convert the metabolite long format into a wider one
+#'
+#' @param data The lipidomics dataset
+#'
+#' @return A wide data frame
+#'
+metabolites_to_wider <- function(data) {
+  data %>%
+    tidyr::pivot_wider(
+      names_from = metabolite,
+      values_from = value,
+      values_fn = mean,
+      names_prefix = "metabolite_"
+    )
+}
+
+
+#' A transformation recipe to pre-process the data
+#'
+#' @param data The lipidomics dataset
+#' @param metabolite_variable The column of the metabolite variable
+#'
+#' @return Dataframe
+#'
+create_recipe_spec <- function(data, metabolite_variable) {
+  recipes::recipe(data) %>%
+    recipes::update_role(
+      {{ metabolite_variable }}, age, gender,
+      new_role = "predictor"
+    ) %>%
+    recipes::update_role(class, new_role = "outcome") %>%
+    recipes::step_normalize(tidyselect::starts_with("metabolite_"))
+}
+
+
+#' Create a workflow object of the model
+#'
+#' @param model_specs The model specs
+#' @param recipe_specs The recipe specs
+#'
+#' @return A workflow object
+#'
+create_model_workflow <- function(model_specs, recipe_specs) {
+  workflows::workflow() %>%
+    workflows::add_model(model_specs) %>%
+    workflows::add_recipe(recipe_specs)
+}
+
+
+#' Create a tidy output of the model results
+#'
+#' @param workflow_fitted_model The model workflow object that has been fitted
+#'
+#' @return Dataframe
+#'
+tidy_model_output <- function(workflow_fitted_model) {
+  workflow_fitted_model %>%
+    workflows::extract_fit_parsnip() %>%
+    broom::tidy(exponentiate = TRUE)
 }
